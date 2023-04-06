5b3250f6ef9a489befd359581dec0b418552c4d6
igor <igorxavier96@gmail.com>
2023-04-06 19:33:23 -0300

    Tag do Section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details(){
 
@@ -16,7 +17,10 @@ export function Details(){
         </Links>
       </Section>
 
-      
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
 
       <Button title="Voltar" />
     </Container>
