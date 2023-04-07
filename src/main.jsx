5db636e5f7a8c4e638a362b7b5d2a345063e9f2a
igor <igorxavier96@gmail.com>
2023-04-07 16:05:43 -0300

    background

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,15 @@ import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
 import { Home } from './pages/Home'
+import { Details } from './pages/Details'
+import { SignIn } from './pages/SignIn'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home /> 
+      <SignIn /> 
     </ThemeProvider>
   </React.StrictMode>,
 )
