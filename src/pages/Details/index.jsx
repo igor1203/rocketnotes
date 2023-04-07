34bccd39ec423b1967cb48192d18e916b89bd1e9
igor <igorxavier96@gmail.com>
2023-04-06 21:00:22 -0300

    finish Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
+
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
@@ -10,22 +11,43 @@ export function Details(){
   return(
     <Container>
       <Header />
+      
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <ButtonText title="Excluir nota" />
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            At veniam quae eos voluptatem maiores sint in excepturi,
+            dolores deleniti iure impedit reprehenderit magni
+            dignissimos repellat aspernatur distinctio dolore ad autem!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            At veniam quae eos voluptatem maiores sint in excepturi,
+            dolores deleniti iure impedit reprehenderit magni
+            dignissimos repellat aspernatur distinctio dolore ad autem!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            At veniam quae eos voluptatem maiores sint in excepturi,
+            dolores deleniti iure impedit reprehenderit magni
+            dignissimos repellat aspernatur distinctio dolore ad autem!
+          </p>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
 
-      <Button title="Voltar" />
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
