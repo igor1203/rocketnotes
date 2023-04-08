f320c6df2fb440497724647187ae69f2e08000c8
igor <igorxavier96@gmail.com>
2023-04-08 11:46:19 -0300

    Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,14 @@ import  theme  from './styles/theme'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-import { Home } from './pages/Home'
-import { Details } from './pages/Details'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New /> 
+      < Routes /> 
     </ThemeProvider>
   </React.StrictMode>,
 )
