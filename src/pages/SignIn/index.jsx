3a05de107743c7bc00535d39421c1c9dfbcc751a
igor <igorxavier96@gmail.com>
2023-04-11 20:41:18 -0300

    Login com sucesso

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useAuth } from "../../hooks/auth";
 import { Background, Container, Form } from "./styles"; 
 import { Link } from "react-router-dom";
@@ -7,9 +8,14 @@ import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function SignIn(){
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const data = useAuth()
-  console.log("MEU CONTEXTO =>", data)
+  const { signIn } = useAuth()
+
+  function handleSignIn(){
+    signIn({ email, password })
+  }
 
   return(
     <Container>
@@ -23,17 +29,19 @@ export function SignIn(){
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
-        <Link to="/register">
+        <Link to="/register" >
           Criar conta
         </Link>
           
