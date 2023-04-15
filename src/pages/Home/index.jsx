0de73e1ed025febc29c282c8eb0cf6d568294fa4
igor <igorxavier96@gmail.com>
2023-04-14 23:36:51 -0300

    carregar notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,8 +12,10 @@ import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
 
 export function Home(){
+  const [search, setSearch] = useState("")
   const [tags, setTags] = useState([])
   const [tagsSelected, setTagsSelected] = useState([])
+  const [notes, setNotes] = useState([])
 
   function handleTagSelected( tagName ){   
     const alreadySelected = tagsSelected.includes(tagName)
@@ -34,7 +36,16 @@ export function Home(){
     }
 
     fetchTags()
-  }, []) 
+  }, [])
+  
+  useEffect(() => {
+    async function fetchNotes(){
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+      setNotes(response.data)
+    }
+
+    fetchNotes()
+  }, [tagsSelected, search])
 
   return(
     <Container>
@@ -66,27 +77,24 @@ export function Home(){
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+        <Input
+          placeholder="Pesquisar pelo título"
+          icon={FiSearch}
+          onChange={() => setSearch(e.target.value)}    
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note data={{
-             title: 'React Modal',
-              tags: [
-                {id: '1', name: 'react'},
-              ] 
-            }}
-          />
+          {
+            notes.map(note => (
 
-          <Note data={{
-             title: 'Exemplo de Middleware',
-              tags: [
-                {id: '2', name: 'express'},
-                {id: '3', name: 'nodejs'} 
-              ] 
-            }}
-          />
+              <Note
+                key={String(note.id)}
+                data={note}
+              />
+            ))
+          }
         </Section>
       </Content>
 
