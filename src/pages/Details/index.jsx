b0a878697fb0ea6d70f5edd2aaf16fc0955a10d3
igor <igorxavier96@gmail.com>
2023-04-15 00:44:02 -0300

    carregando detalhes da nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,8 @@
 import { Container, Links, Content } from './styles'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+import { api } from '../../services/api'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
@@ -8,46 +12,83 @@ import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
 
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack(){
+    navigate("/")
+  }
+
+  useEffect(() => {
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
   return(
     <Container>
       <Header />
       
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
+      {
+        data &&
+     
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
 
-          <h1>
-            Introdução ao React
-          </h1>
+            <h1>
+              {data.title}
+            </h1>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            At veniam quae eos voluptatem maiores sint in excepturi,
-            dolores deleniti iure impedit reprehenderit magni
-            dignissimos repellat aspernatur distinctio dolore ad autem!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            At veniam quae eos voluptatem maiores sint in excepturi,
-            dolores deleniti iure impedit reprehenderit magni
-            dignissimos repellat aspernatur distinctio dolore ad autem!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            At veniam quae eos voluptatem maiores sint in excepturi,
-            dolores deleniti iure impedit reprehenderit magni
-            dignissimos repellat aspernatur distinctio dolore ad autem!
-          </p>
+            <p> 
+              {data.description}            
+             </p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li><a href="#">https://www.rocketseat.com.br</a></li>
-              <li><a href="#">https://www.rocketseat.com.br</a></li>
-            </Links>
-          </Section>
+             {
+              data.links &&
+                <Section title="Links úteis">
+                  <Links>
+                    {
+                      data.links.map(link => (
+                        <li key={String(link.id)}>
+                         <a href={link.url} target='_blank'>
+                              {link.url}
+                          </a>
+                        </li>
+                      ))
+                    }
+                  </Links>
+                </Section>
+             }
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="nodejs" />
-          </Section>
 
-          <Button title="Voltar" />
-        </Content>
-      </main>
+             {
+              data.tags && 
+            <Section title="Marcadores">
+              {
+                data.tags.map(tag => (
+                  <Tag
+                    key={String(tag.id)}
+                    title={tag.name} 
+                  />
+                ))
+              }
+            </Section>
+
+             }
+
+            <Button 
+              title="Voltar" 
+              onClick={handleBack} 
+            />
+          </Content>
+        </main>
+       }
     </Container>
   )
 }
